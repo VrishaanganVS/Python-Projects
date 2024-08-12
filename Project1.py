e12c7eda8dde28850ccdf15fd86db981890025c2
VrishaanganVS <143398184+VrishaanganVS@users.noreply.github.com>
2024-08-12 08:18:04 +0000

    update

--- Project1.py
+++ Project1.py
@@ -9,10 +9,16 @@ import math
 random_number = random.randint(1,100)
 print("A Number has been set by the system, The user may try to guess it now")
 
-u = int(input("Enter the number to guess"))
+u = int(input("Enter the number to guess:"))
 
-if u<random_number:
+i=1
+count = 0
+
+while u<random_number:
     print("Value smaller than number")
+    count=count+i
+    
+        
 elif u>random_number:
     print("Value Greater than number")
 elif u==random_number:
