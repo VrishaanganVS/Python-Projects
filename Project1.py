19a5e3214bad4a73dba829420f0457bda37768a8
VrishaanganVS <143398184+VrishaanganVS@users.noreply.github.com>
2024-08-12 09:52:50 +0000

    update

--- Project1.py
+++ Project1.py
@@ -6,6 +6,7 @@ Then we have to get user input for guessing the number
 import random
 import math
 
+max_attempts= 10
 random_number = random.randint(1,100)
 print("A Number has been set by the system, The user may try to guess it now")
 
@@ -13,11 +14,21 @@ def user():
     nu = int(input("Enter number to guess:"))
 
 def guess(nu,random_number):
+    while nu != random_number:
         if nu == random_number:
             print("Guessed correct")
         elif nu<random_number:
-            return "Guessed too small"
+            print("Guessed too small")
+            guess = int(input("Enter number again:"))
         else:
-            return "Too high"
+            print("Too high")
+            guess = int(input("Enter number again:"))
 
-    
+
+def att():
+    attempts = 0
+    won = False
+   
+    while attempts < max_attempts:
+        attempts+= 1
+        myg= guess()
